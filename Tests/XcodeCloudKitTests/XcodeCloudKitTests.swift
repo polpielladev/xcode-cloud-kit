56e6a914c69066e96903be4af474e92832b3a25c
Pol Piella Abadia <hi@polpiella.dev>
2023-04-09 22:04:06 +0200

    Test product response with no repositories does not include a product

--- Tests/XcodeCloudKitTests/XcodeCloudKitTests.swift
+++ Tests/XcodeCloudKitTests/XcodeCloudKitTests.swift
@@ -1,6 +1,32 @@
 import XCTest
 @testable import XcodeCloudKit
+import AppStoreConnect_Swift_SDK
+
+class MockAppStoreConnectAPIClient: AppStoreConnectAPIClient {
+    var productsResponseToReturn: CiProductsResponse?
+    func allProducts() async throws -> CiProductsResponse {
+        try XCTUnwrap(productsResponseToReturn)
+    }
+}
 
 final class XcodeCloudKitTests: XCTestCase {
-
+    func test_GivenASCClientReturnsProductWithNoRepository_ThenProductIsNotInTheArray() async throws {
+        let client = MockAppStoreConnectAPIClient()
+        let sut = DefaultXcodeCloudKit(client: client)
+        client.productsResponseToReturn = noRepositoriesProductResponse()
+        
+        let products = try await sut.allProducts()
+        
+        XCTAssertTrue(products.isEmpty)
+    }
+    
+    private func noRepositoriesProductResponse() -> CiProductsResponse {
+        return CiProductsResponse(data: [
+            .init(
+                type: .ciProducts,
+                id: "no-repositories",
+                attributes: .init(name: "No Repositories Response"),
+                links: .init(this: ""))
+        ], links: .init(this: ""))
+    }
 }
