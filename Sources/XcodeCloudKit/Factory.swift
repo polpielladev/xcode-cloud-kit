def81c559f4fd09f99cd922d0587a88ccd8d768a
Pol Piella Abadia <hi@polpiella.dev>
2023-04-09 22:31:16 +0200

    Creates a factory method

--- Sources/XcodeCloudKit/Factory.swift
+++ Sources/XcodeCloudKit/Factory.swift
@@ -1,1 +1,11 @@
-import Foundation
+import AppStoreConnect_Swift_SDK
+
+public enum Factory {
+    public static func make(issuerID: String, privateKeyID: String, privateKey: String) throws -> XcodeCloudKit {
+        let configuration = try APIConfiguration(issuerID: issuerID, privateKeyID: privateKeyID, privateKey: privateKey)
+        let provider = APIProvider(configuration: configuration)
+        let client = AppStoreConnectSDKAdapter(appStoreConnectSDK: provider)
+        
+        return DefaultXcodeCloudKit(client: client)
+    }
+}
